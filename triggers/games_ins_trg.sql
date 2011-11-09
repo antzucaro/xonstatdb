764db09a6e620f25519cd05da63f7740c857f6f4
Ant Zucaro <azucaro@gmail.com>
2011-11-09 18:15:55 -0500

    Fix the triggers for partitioning.

--- triggers/games_ins_trg.sql
+++ triggers/games_ins_trg.sql
@@ -1,23 +1,23 @@
-CREATE OR REPLACE FUNCTION games_ins()
+﻿CREATE OR REPLACE FUNCTION games_ins()
 RETURNS TRIGGER AS $$
 BEGIN
-    IF (create_dt >= DATE '2011-04-01' AND create_dt < DATE '2011-07-01') THEN
+    IF (NEW.create_dt >= DATE '2011-04-01' AND NEW.create_dt < DATE '2011-07-01') THEN
         INSERT INTO games_2011Q2 VALUES (NEW.*);
-    ELSIF (create_dt >= DATE '2011-07-01' AND create_dt < DATE '2011-10-01') THEN
+    ELSIF (NEW.create_dt >= DATE '2011-07-01' AND NEW.create_dt < DATE '2011-10-01') THEN
         INSERT INTO games_2011Q3 VALUES (NEW.*);
-    ELSIF (create_dt >= DATE '2011-10-01' AND create_dt < DATE '2012-01-01') THEN
+    ELSIF (NEW.create_dt >= DATE '2011-10-01' AND NEW.create_dt < DATE '2012-01-01') THEN
         INSERT INTO games_2011Q4 VALUES (NEW.*);
-    ELSIF (create_dt >= DATE '2012-01-01' AND create_dt < DATE '2012-04-01') THEN
+    ELSIF (NEW.create_dt >= DATE '2012-01-01' AND NEW.create_dt < DATE '2012-04-01') THEN
         INSERT INTO games_2012Q1 VALUES (NEW.*);
-    ELSIF (create_dt >= DATE '2012-04-01' AND create_dt < DATE '2012-07-01') THEN
+    ELSIF (NEW.create_dt >= DATE '2012-04-01' AND NEW.create_dt < DATE '2012-07-01') THEN
         INSERT INTO games_2012Q2 VALUES (NEW.*);
-    ELSIF (create_dt >= DATE '2012-07-01' AND create_dt < DATE '2012-10-01') THEN
+    ELSIF (NEW.create_dt >= DATE '2012-07-01' AND NEW.create_dt < DATE '2012-10-01') THEN
         INSERT INTO games_2012Q3 VALUES (NEW.*);
-    ELSIF (create_dt >= DATE '2012-10-01' AND create_dt < DATE '2013-01-01') THEN
+    ELSIF (NEW.create_dt >= DATE '2012-10-01' AND NEW.create_dt < DATE '2013-01-01') THEN
         INSERT INTO games_2012Q4 VALUES (NEW.*);
-    ELSIF (create_dt >= DATE '2013-01-01' AND create_dt < DATE '2013-04-01') THEN
+    ELSIF (NEW.create_dt >= DATE '2013-01-01' AND NEW.create_dt < DATE '2013-04-01') THEN
         INSERT INTO games_2013Q1 VALUES (NEW.*);
-    ELSIF (create_dt >= DATE '2013-04-01' AND create_dt < DATE '2013-07-01') THEN
+    ELSIF (NEW.create_dt >= DATE '2013-04-01' AND NEW.create_dt < DATE '2013-07-01') THEN
         INSERT INTO games_2013Q2 VALUES (NEW.*);
     ELSE
         RAISE EXCEPTION 'Date out of range.  Fix the games_ins() trigger!';
@@ -26,3 +26,8 @@ BEGIN
 END;
 $$
 LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS games_ins_trg ON xonstat.games;
+CREATE TRIGGER games_ins_trg
+    BEFORE INSERT on xonstat.games
+    FOR EACH ROW EXECUTE PROCEDURE games_ins();
--- triggers/player_weapon_stats_ins_trg.sql
+++ triggers/player_weapon_stats_ins_trg.sql
@@ -1,23 +1,23 @@
-CREATE OR REPLACE FUNCTION player_weapon_stats_ins()
+﻿CREATE OR REPLACE FUNCTION player_weapon_stats_ins()
 RETURNS TRIGGER AS $$
 BEGIN
-    IF (create_dt >= DATE '2011-04-01' AND create_dt < DATE '2011-07-01') THEN
+    IF (NEW.create_dt >= DATE '2011-04-01' AND NEW.create_dt < DATE '2011-07-01') THEN
         INSERT INTO player_weapon_stats_2011Q2 VALUES (NEW.*);
-    ELSIF (create_dt >= DATE '2011-07-01' AND create_dt < DATE '2011-10-01') THEN
+    ELSIF (NEW.create_dt >= DATE '2011-07-01' AND NEW.create_dt < DATE '2011-10-01') THEN
         INSERT INTO player_weapon_stats_2011Q3 VALUES (NEW.*);
-    ELSIF (create_dt >= DATE '2011-10-01' AND create_dt < DATE '2012-01-01') THEN
+    ELSIF (NEW.create_dt >= DATE '2011-10-01' AND NEW.create_dt < DATE '2012-01-01') THEN
         INSERT INTO player_weapon_stats_2011Q4 VALUES (NEW.*);
-    ELSIF (create_dt >= DATE '2012-01-01' AND create_dt < DATE '2012-04-01') THEN
+    ELSIF (NEW.create_dt >= DATE '2012-01-01' AND NEW.create_dt < DATE '2012-04-01') THEN
         INSERT INTO player_weapon_stats_2012Q1 VALUES (NEW.*);
-    ELSIF (create_dt >= DATE '2012-04-01' AND create_dt < DATE '2012-07-01') THEN
+    ELSIF (NEW.create_dt >= DATE '2012-04-01' AND NEW.create_dt < DATE '2012-07-01') THEN
         INSERT INTO player_weapon_stats_2012Q2 VALUES (NEW.*);
-    ELSIF (create_dt >= DATE '2012-07-01' AND create_dt < DATE '2012-10-01') THEN
+    ELSIF (NEW.create_dt >= DATE '2012-07-01' AND NEW.create_dt < DATE '2012-10-01') THEN
         INSERT INTO player_weapon_stats_2012Q3 VALUES (NEW.*);
-    ELSIF (create_dt >= DATE '2012-10-01' AND create_dt < DATE '2013-01-01') THEN
+    ELSIF (NEW.create_dt >= DATE '2012-10-01' AND NEW.create_dt < DATE '2013-01-01') THEN
         INSERT INTO player_weapon_stats_2012Q4 VALUES (NEW.*);
-    ELSIF (create_dt >= DATE '2013-01-01' AND create_dt < DATE '2013-04-01') THEN
+    ELSIF (NEW.create_dt >= DATE '2013-01-01' AND NEW.create_dt < DATE '2013-04-01') THEN
         INSERT INTO player_weapon_stats_2013Q1 VALUES (NEW.*);
-    ELSIF (create_dt >= DATE '2013-04-01' AND create_dt < DATE '2013-07-01') THEN
+    ELSIF (NEW.create_dt >= DATE '2013-04-01' AND NEW.create_dt < DATE '2013-07-01') THEN
         INSERT INTO player_weapon_stats_2013Q2 VALUES (NEW.*);
     ELSE
         RAISE EXCEPTION 'Date out of range.  Fix the player_weapon_stats_ins() trigger!';
@@ -26,3 +26,8 @@ BEGIN
 END;
 $$
 LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS player_weapon_stats_ins_trg ON xonstat.player_weapon_stats;
+CREATE TRIGGER player_weapon_stats_ins_trg
+    BEFORE INSERT on xonstat.player_weapon_stats
+    FOR EACH ROW EXECUTE PROCEDURE player_weapon_stats_ins();
