e2d305b32225ec349c763c47b2b36a7662369abb
Ant Zucaro <azucaro@gmail.com>
2012-01-22 12:35:41 -0500

    Save rank history and also do not generate ranks if < 32 games played for the game type.

--- scripts/update_ranks.sql
+++ scripts/update_ranks.sql
@@ -1,4 +1,10 @@
 begin;
+    -- save the history
+    insert into player_ranks_history
+    select * from player_ranks;
+
+    -- get rid of the existing ranks and refresh them using
+    -- the latest elo information for each game type
     delete from player_ranks;
 
     insert into player_ranks(player_id, nick, game_type_cd, elo, rank)
