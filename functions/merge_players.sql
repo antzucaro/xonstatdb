b18626a94d1babcf4079f503c41cbafbb0106f41
Ant Zucaro <azucaro@gmail.com>
2012-02-29 07:05:05 -0500

    Function to merge two players.

---
+++ functions/merge_players.sql
@@ -0,0 +1,34 @@
+﻿create or replace function merge_players(p_winner_player_id players.player_id%TYPE, p_loser_player_id players.player_id%TYPE) RETURNS void as
+$$
+declare
+   rowcount integer;   
+begin
+   -- start with weapon stats
+   update player_weapon_stats
+   set player_id = p_winner_player_id
+   where player_id = p_loser_player_id;
+
+   get diagnostics rowcount = ROW_COUNT;
+   raise notice '% weapon stat rows updated.', rowcount;
+
+   -- then game stats
+   update player_game_stats
+   set player_id = p_winner_player_id
+   where player_id = p_loser_player_id;
+
+   get diagnostics rowcount = ROW_COUNT;
+   raise notice '% game stat rows updated.', rowcount;
+
+   -- then hashkeys (winner takes the loser's hashkey)
+   update hashkeys
+   set player_id = p_winner_player_id
+   where player_id = p_loser_player_id;
+   
+   get diagnostics rowcount = ROW_COUNT;
+   raise notice '% hashkeys copied.', rowcount;
+
+   -- and finally deactivate the old player record
+   update players set active_ind = false where player_id = p_loser_player_id;
+end;
+$$
+language plpgsql;
