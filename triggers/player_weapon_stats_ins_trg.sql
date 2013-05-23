c52ae86477d2842fd62e16c3b879e69206028a23
Ant Zucaro <azucaro@gmail.com>
2013-05-22 22:05:06 -0400

    Partition player weapon stats through 2014.

--- triggers/player_weapon_stats_ins_trg.sql
+++ triggers/player_weapon_stats_ins_trg.sql
@@ -1,24 +1,23 @@
 CREATE OR REPLACE FUNCTION player_weapon_stats_ins()
 RETURNS TRIGGER AS $$
 BEGIN
-    IF (NEW.create_dt >= DATE '2011-04-01' AND NEW.create_dt < DATE '2011-07-01') THEN
-        INSERT INTO player_weapon_stats_2011Q2 VALUES (NEW.*);
-    ELSIF (NEW.create_dt >= DATE '2011-07-01' AND NEW.create_dt < DATE '2011-10-01') THEN
-        INSERT INTO player_weapon_stats_2011Q3 VALUES (NEW.*);
-    ELSIF (NEW.create_dt >= DATE '2011-10-01' AND NEW.create_dt < DATE '2012-01-01') THEN
-        INSERT INTO player_weapon_stats_2011Q4 VALUES (NEW.*);
-    ELSIF (NEW.create_dt >= DATE '2012-01-01' AND NEW.create_dt < DATE '2012-04-01') THEN
-        INSERT INTO player_weapon_stats_2012Q1 VALUES (NEW.*);
-    ELSIF (NEW.create_dt >= DATE '2012-04-01' AND NEW.create_dt < DATE '2012-07-01') THEN
-        INSERT INTO player_weapon_stats_2012Q2 VALUES (NEW.*);
-    ELSIF (NEW.create_dt >= DATE '2012-07-01' AND NEW.create_dt < DATE '2012-10-01') THEN
-        INSERT INTO player_weapon_stats_2012Q3 VALUES (NEW.*);
-    ELSIF (NEW.create_dt >= DATE '2012-10-01' AND NEW.create_dt < DATE '2013-01-01') THEN
-        INSERT INTO player_weapon_stats_2012Q4 VALUES (NEW.*);
-    ELSIF (NEW.create_dt >= DATE '2013-01-01' AND NEW.create_dt < DATE '2013-04-01') THEN
-        INSERT INTO player_weapon_stats_2013Q1 VALUES (NEW.*);
-    ELSIF (NEW.create_dt >= DATE '2013-04-01' AND NEW.create_dt < DATE '2013-07-01') THEN
+    IF (NEW.create_dt >= DATE '2013-04-01' AND NEW.create_dt < DATE '2013-07-01') THEN
         INSERT INTO player_weapon_stats_2013Q2 VALUES (NEW.*);
+    ELSIF (NEW.create_dt >= DATE '2013-07-01' AND NEW.create_dt < DATE '2013-10-01') THEN
+        INSERT INTO player_weapon_stats_2013Q3 VALUES (NEW.*);
+    ELSIF (NEW.create_dt >= DATE '2013-10-01' AND NEW.create_dt < DATE '2014-01-01') THEN
+        INSERT INTO player_weapon_stats_2013Q4 VALUES (NEW.*);
+
+    -- 2014
+    ELSIF (NEW.create_dt >= DATE '2014-01-01' AND NEW.create_dt < DATE '2014-04-01') THEN
+        INSERT INTO player_weapon_stats_2014Q1 VALUES (NEW.*);
+    ELSIF (NEW.create_dt >= DATE '2014-04-01' AND NEW.create_dt < DATE '2014-07-01') THEN
+        INSERT INTO player_weapon_stats_2014Q2 VALUES (NEW.*);
+    ELSIF (NEW.create_dt >= DATE '2014-07-01' AND NEW.create_dt < DATE '2014-10-01') THEN
+        INSERT INTO player_weapon_stats_2014Q3 VALUES (NEW.*);
+    ELSIF (NEW.create_dt >= DATE '2014-10-01' AND NEW.create_dt < DATE '2015-01-01') THEN
+        INSERT INTO player_weapon_stats_2014Q4 VALUES (NEW.*);
+
     ELSE
         RAISE EXCEPTION 'Date out of range.  Fix the player_weapon_stats_ins() trigger!';
     END IF;
