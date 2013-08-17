147c48a5718e9c96a58230268fd307d0af02140d
antzucaro <azucaro@gmail.com>
2013-08-17 00:34:33 -0400

    Damn off-by-one errors. *shakes fist*

--- queries/show_elo_rot.sql
+++ queries/show_elo_rot.sql
@@ -5,8 +5,8 @@ select
   update_dt last_update_dt, 
   (current_date - (update_dt::date)) number_of_days, 
   (current_date - (update_dt::date))-30 number_of_days_past_30, 
-  (current_date - (update_dt::date)-31)/7 penalty,
-  greatest(100, elo - ((current_date - (update_dt::date)-31)/7)) new_elo
+  (current_date - (update_dt::date)-31)/7+1 penalty,
+  greatest(100, elo - ((current_date - (update_dt::date)-31)/7+1)) new_elo
 from player_elos
 where 
   update_dt < (current_timestamp at time zone 'UTC' - interval '30 days')
--- scripts/update_elos.sql
+++ scripts/update_elos.sql
@@ -1,6 +1,6 @@
 begin;
   update player_elos
-  set elo=greatest(100, elo - ((current_date - (update_dt::date)-31)/7))
+  set elo=greatest(100, elo - ((current_date - (update_dt::date)-31)/7+1))
   where update_dt < (current_timestamp at time zone 'UTC' - interval '30 days')
   and elo != 100;
 end;
