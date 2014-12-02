6477b691162a5add0fcd578c9c62002d3fa3cdd3
Ant Zucaro <azucaro@gmail.com>
2014-12-01 21:18:57 -0500

    Partition tables out to 2020, remove old ones.
    Since these scripts are used to create a new xonstatdb, references to
    partitions using past dates will just mean empty tables. I'll cut off this
    pruning at each prior year.

--- triggers/player_game_stats_ins_trg.sql
+++ triggers/player_game_stats_ins_trg.sql
@@ -1,31 +1,67 @@
 CREATE OR REPLACE FUNCTION player_game_stats_ins()
 RETURNS TRIGGER AS $$
 BEGIN
-    -- 2013
-    IF (NEW.create_dt >= DATE '2013-01-01' AND NEW.create_dt < DATE '2013-04-01') THEN
-        INSERT INTO player_game_stats_2013Q1 VALUES (NEW.*);
-    ELSIF (NEW.create_dt >= DATE '2013-04-01' AND NEW.create_dt < DATE '2013-07-01') THEN
-        INSERT INTO player_game_stats_2013Q2 VALUES (NEW.*);
-    ELSIF (NEW.create_dt >= DATE '2013-07-01' AND NEW.create_dt < DATE '2013-10-01') THEN
-        INSERT INTO player_game_stats_2013Q3 VALUES (NEW.*);
-    ELSIF (NEW.create_dt >= DATE '2013-10-01' AND NEW.create_dt < DATE '2014-01-01') THEN
-        INSERT INTO player_game_stats_2013Q4 VALUES (NEW.*);
-
-    -- 2014
-    ELSIF (NEW.create_dt >= DATE '2014-01-01' AND NEW.create_dt < DATE '2014-04-01') THEN
-        INSERT INTO player_game_stats_2014Q1 VALUES (NEW.*);
-    ELSIF (NEW.create_dt >= DATE '2014-04-01' AND NEW.create_dt < DATE '2014-07-01') THEN
-        INSERT INTO player_game_stats_2014Q2 VALUES (NEW.*);
-    ELSIF (NEW.create_dt >= DATE '2014-07-01' AND NEW.create_dt < DATE '2014-10-01') THEN
-        INSERT INTO player_game_stats_2014Q3 VALUES (NEW.*);
-    ELSIF (NEW.create_dt >= DATE '2014-10-01' AND NEW.create_dt < DATE '2015-01-01') THEN
-        INSERT INTO player_game_stats_2014Q4 VALUES (NEW.*);
-
-    ELSE
-        RAISE EXCEPTION 'Date out of range.  Fix the player_game_stats_ins() trigger!';
-    END IF;
-    RETURN NULL;
-END;
+	IF (NEW.create_dt >= DATE '2014-01-01' AND NEW.create_dt < DATE '2014-04-01') THEN
+		INSERT INTO player_game_stats_2014Q1 VALUES (NEW.*);
+	ELSIF (NEW.create_dt >= DATE '2014-04-01' AND NEW.create_dt < DATE '2014-07-01') THEN
+		INSERT INTO player_game_stats_2014Q2 VALUES (NEW.*);
+	ELSIF (NEW.create_dt >= DATE '2014-07-01' AND NEW.create_dt < DATE '2014-10-01') THEN
+		INSERT INTO player_game_stats_2014Q3 VALUES (NEW.*);
+	ELSIF (NEW.create_dt >= DATE '2014-10-01' AND NEW.create_dt < DATE '2015-01-01') THEN
+		INSERT INTO player_game_stats_2014Q4 VALUES (NEW.*);
+	ELSIF (NEW.create_dt >= DATE '2015-01-01' AND NEW.create_dt < DATE '2015-04-01') THEN
+		INSERT INTO player_game_stats_2015Q1 VALUES (NEW.*);
+	ELSIF (NEW.create_dt >= DATE '2015-04-01' AND NEW.create_dt < DATE '2015-07-01') THEN
+		INSERT INTO player_game_stats_2015Q2 VALUES (NEW.*);
+	ELSIF (NEW.create_dt >= DATE '2015-07-01' AND NEW.create_dt < DATE '2015-10-01') THEN
+		INSERT INTO player_game_stats_2015Q3 VALUES (NEW.*);
+	ELSIF (NEW.create_dt >= DATE '2015-10-01' AND NEW.create_dt < DATE '2016-01-01') THEN
+		INSERT INTO player_game_stats_2015Q4 VALUES (NEW.*);
+	ELSIF (NEW.create_dt >= DATE '2016-01-01' AND NEW.create_dt < DATE '2016-04-01') THEN
+		INSERT INTO player_game_stats_2016Q1 VALUES (NEW.*);
+	ELSIF (NEW.create_dt >= DATE '2016-04-01' AND NEW.create_dt < DATE '2016-07-01') THEN
+		INSERT INTO player_game_stats_2016Q2 VALUES (NEW.*);
+	ELSIF (NEW.create_dt >= DATE '2016-07-01' AND NEW.create_dt < DATE '2016-10-01') THEN
+		INSERT INTO player_game_stats_2016Q3 VALUES (NEW.*);
+	ELSIF (NEW.create_dt >= DATE '2016-10-01' AND NEW.create_dt < DATE '2017-01-01') THEN
+		INSERT INTO player_game_stats_2016Q4 VALUES (NEW.*);
+	ELSIF (NEW.create_dt >= DATE '2017-01-01' AND NEW.create_dt < DATE '2017-04-01') THEN
+		INSERT INTO player_game_stats_2017Q1 VALUES (NEW.*);
+	ELSIF (NEW.create_dt >= DATE '2017-04-01' AND NEW.create_dt < DATE '2017-07-01') THEN
+		INSERT INTO player_game_stats_2017Q2 VALUES (NEW.*);
+	ELSIF (NEW.create_dt >= DATE '2017-07-01' AND NEW.create_dt < DATE '2017-10-01') THEN
+		INSERT INTO player_game_stats_2017Q3 VALUES (NEW.*);
+	ELSIF (NEW.create_dt >= DATE '2017-10-01' AND NEW.create_dt < DATE '2018-01-01') THEN
+		INSERT INTO player_game_stats_2017Q4 VALUES (NEW.*);
+	ELSIF (NEW.create_dt >= DATE '2018-01-01' AND NEW.create_dt < DATE '2018-04-01') THEN
+		INSERT INTO player_game_stats_2018Q1 VALUES (NEW.*);
+	ELSIF (NEW.create_dt >= DATE '2018-04-01' AND NEW.create_dt < DATE '2018-07-01') THEN
+		INSERT INTO player_game_stats_2018Q2 VALUES (NEW.*);
+	ELSIF (NEW.create_dt >= DATE '2018-07-01' AND NEW.create_dt < DATE '2018-10-01') THEN
+		INSERT INTO player_game_stats_2018Q3 VALUES (NEW.*);
+	ELSIF (NEW.create_dt >= DATE '2018-10-01' AND NEW.create_dt < DATE '2019-01-01') THEN
+		INSERT INTO player_game_stats_2018Q4 VALUES (NEW.*);
+	ELSIF (NEW.create_dt >= DATE '2019-01-01' AND NEW.create_dt < DATE '2019-04-01') THEN
+		INSERT INTO player_game_stats_2019Q1 VALUES (NEW.*);
+	ELSIF (NEW.create_dt >= DATE '2019-04-01' AND NEW.create_dt < DATE '2019-07-01') THEN
+		INSERT INTO player_game_stats_2019Q2 VALUES (NEW.*);
+	ELSIF (NEW.create_dt >= DATE '2019-07-01' AND NEW.create_dt < DATE '2019-10-01') THEN
+		INSERT INTO player_game_stats_2019Q3 VALUES (NEW.*);
+	ELSIF (NEW.create_dt >= DATE '2019-10-01' AND NEW.create_dt < DATE '2020-01-01') THEN
+		INSERT INTO player_game_stats_2019Q4 VALUES (NEW.*);
+	ELSIF (NEW.create_dt >= DATE '2020-01-01' AND NEW.create_dt < DATE '2020-04-01') THEN
+		INSERT INTO player_game_stats_2020Q1 VALUES (NEW.*);
+	ELSIF (NEW.create_dt >= DATE '2020-04-01' AND NEW.create_dt < DATE '2020-07-01') THEN
+		INSERT INTO player_game_stats_2020Q2 VALUES (NEW.*);
+	ELSIF (NEW.create_dt >= DATE '2020-07-01' AND NEW.create_dt < DATE '2020-10-01') THEN
+		INSERT INTO player_game_stats_2020Q3 VALUES (NEW.*);
+	ELSIF (NEW.create_dt >= DATE '2020-10-01' AND NEW.create_dt < DATE '2021-01-01') THEN
+		INSERT INTO player_game_stats_2020Q4 VALUES (NEW.*);
+	ELSE
+		RAISE EXCEPTION 'Date out of range. Fix the player_game_stats_ins() trigger!';
+	END IF;
+	RETURN NULL;
+END
 $$
 LANGUAGE plpgsql;
 
