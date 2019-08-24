9da849fd765f5ea4578d81bde1467b32afa684a7
Ant Zucaro <azucaro@gmail.com>
2019-08-24 07:53:14 -0400

    Update the RPC with what it stands for.

--- build/build_full.sql
+++ build/build_full.sql
@@ -138,7 +138,7 @@ insert into cd_weapon(weapon_cd, descr) values('okhmg', 'Overkill Heavy Machine 
 insert into cd_weapon(weapon_cd, descr) values('oknex', 'Overkill Nex');
 insert into cd_weapon(weapon_cd, descr) values('okshotgun', 'Overkill Shotgun');
 insert into cd_weapon(weapon_cd, descr) values('okmachinegun', 'Overkill Machine Gun');
-insert into cd_weapon(weapon_cd, descr) values('okrpc', 'Overkill RPC');
+insert into cd_weapon(weapon_cd, descr) values('okrpc', 'Overkill Rocket-Propelled Chainsaw');
 
 -- achievements
 insert into cd_achievement(achievement_cd, descr, active_ind)
